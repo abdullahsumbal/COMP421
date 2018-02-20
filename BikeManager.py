8889bdf80ddce68147d9082209fc79438885a958
abdullahsumbal <abdullahsumbal@gmail.com>
2018-02-20 15:39:08 -0800

    p

--- BikeManager.py
+++ BikeManager.py
@@ -1,12 +1,14 @@
 import psycopg2
+import csv
 
-db = psycopg2.connect(host="comp421.cs.mcgill.ca",database="cs421", user="cs421g29", password="spinBike4!")
+# Connection to the database
+db = psycopg2.connect(host="comp421.cs.mcgill.ca",database="cs421", user="cs421g29", password="XXX")
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
 # execute SQL query using execute() method.
-cursor.execute("INSERT INTO bikemanagers (bmemail , bmname , bmpassword) VALUES ('muhammad.sumbal@mail.mcgill.ca', 'Sumbal', 'yolo');")
+cursor.execute("INSERT INTO bikemanagers (bmemail , bmname , bmpassword) VALUES (%s,%s,%s);", ('muhammad1.sumbal@mail.mcgill.ca', 'Sumbal', 'yolo'))
 
 # Fetch a single row using fetchone() method.
 #data = cursor.fetchone()
